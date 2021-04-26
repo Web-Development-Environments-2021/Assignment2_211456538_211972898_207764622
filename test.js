62fc486f00146c6767d75753824ed33c002fb194
GuyArieli17 <guyarieli17@gmail.com>
2021-04-26 22:49:38 +0300

    add test and fix A*

--- test.js
+++ test.js
@@ -1,3 +1,21 @@
+class Node{
+    constructor(position,g,h){
+        this.position = position
+        this.g = g;
+        this.h = h;
+        this.f = this.h + this.g;
+        this.prev = null;
+    }
+
+    compare(other){
+        let [y1,x1] = this.position;
+        let [y2,x2] = other.position;
+        if(y1 === y2 && x1 == x2) return 0;
+        else if(this.f > other.f) return 1;
+        else return -1;
+    }
+}
+
 class AStar{
     constructor(board){
         this.board = board;
